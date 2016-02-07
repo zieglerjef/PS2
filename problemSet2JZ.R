c76b0769e79ec0ba7d1f1c08e7304472ddad97ab
zieglerjef <zieglerjef@gmail.com>
2016-02-07 16:41:01 -0600

    Add function 2

--- problemSet2JZ.R
+++ problemSet2JZ.R
@@ -1,6 +1,6 @@
-#################################
-### 1) Calculating violations ###
-#################################
+##############################
+### Calculating violations ###
+##############################
 
 # Create function that:
 # - takes in vector or matrix
@@ -8,7 +8,7 @@
 # - output also contains full digit distribution
 # Extra:
 # - accomodates any input length
-# - throws error if input non-integer
+# - throws error if input non-integer or numeric
 
 violations <- function(x, m=TRUE, d=TRUE){
   # check input type
@@ -37,12 +37,14 @@ violations <- function(x, m=TRUE, d=TRUE){
     print(freq)
     cat("\n Leemis' m Statistic: \n")
     print(mStat)
+    invisible(list("mStat" = mStat))
   }
   if(m==FALSE & d==TRUE){
     cat("\n Table of Proportions: \n")
     print(freq)
     cat("\n Cho-Gain's d: \n")
     print(dStat)
+    invisible(list("dStat" = dStat))
   }
   if(m==TRUE & d==TRUE){
     cat("\n Table of Proportions: \n")
@@ -51,6 +53,7 @@ violations <- function(x, m=TRUE, d=TRUE){
     print(mStat)
     cat("\n Cho-Gain's d: \n")
     print(dStat)
+    invisible(list("mStat" = mStat, "dStat" = dStat))
   }
 }
 
@@ -59,7 +62,7 @@ violations <- function(x, m=TRUE, d=TRUE){
 # imput: character vector
 x1 <- as.character(sample(1:99, size=10, replace=T))
 violations(x1, m=T, d=F)
-
+# output: error
 
 # imput: vector, length 10
 x2 <- sample(1:99, size=10, replace=T)
@@ -75,6 +78,115 @@ violations(x3, m=F, d=T)
 x4 <- matrix(sample(1:99, size=20, replace=T),
              ncol=2)
 # m=T, d=T
-violations(x4, m=T, d=T)
+y <- violations(x4, m=T, d=T)
 # m=F, d=F
 violations(x4, m=F, d=F)
+
+
+
+#######################
+### Critical values ###
+#######################
+
+print.benfords <- function(x){
+  # run violations function on input
+  testStat <- violations(x)
+  
+  # create significance asterisk's
+  # create empty vectors
+  alphaM <- NULL
+  alphaD<- NULL
+  
+  # fill in asterisks if critical value is large enough
+  if(testStat$mStat <= 0.851){alphaM <- "Not significant!"}
+  if(testStat$mStat > 0.851){alphaM <- "*"}
+  if(testStat$mStat > 0.967){alphaM <- "**"}
+  if(testStat$mStat > 1.212){alphaM <- "***"}
+  
+  if(testStat$dStat <= 1.212){alphaD <- "Not significant!"}
+  if(testStat$dStat > 1.212){alphaD <- "*"}
+  if(testStat$dStat > 1.330){alphaD <- "**"}
+  if(testStat$dStat > 1.569){alphaD <- "***"}
+  
+  # print:
+  # - critical value
+  # - test statistic name
+  # - how stat was calculated
+  # - significance
+  cat("\n Critical Values \n")
+  
+  print(data.frame("Statistic"=c("Leemis' m", "Cho-Gains' d"),
+                   "Value"=c(testStat$mStat, testStat$dStat),
+                   "Alpha"=c(alphaM, alphaD),
+                   "Calculation"=c("max(freq - log10(1+1/1:9))", "sqrt(sum((freq - log10(1+1/1:9)^2)))")))
+                                                        
+  # print legend explanation
+  cat("\n Note: p < 0.1^{*}; p < 0.05^{**}; p < 0.01^{***} \n")
+}
+
+### test different imputs ###
+
+# imput: character vector
+print.benfords(x1)
+# output: error
+
+# imput: vector, length 10
+print.benfords(x2)
+
+# imput: vector, length = 50
+print.benfords(x3)
+
+# imput: matrix, length = 20
+print.benfords(x4)
+
+
+
+##############################
+### Export critical values ###
+##############################
+
+
+print.benfords2 <- function(x){
+  # run violations function on input
+  testStat <- violations(x)
+  
+  # create significance asterisk's
+  # create empty vectors
+  alphaM <- NULL
+  alphaD<- NULL
+  
+  # fill in asterisks if critical value is large enough
+  if(testStat$mStat <= 0.851){alphaM <- "Not significant!"}
+  if(testStat$mStat > 0.851){alphaM <- "*"}
+  if(testStat$mStat > 0.967){alphaM <- "**"}
+  if(testStat$mStat > 1.212){alphaM <- "***"}
+  
+  if(testStat$dStat <= 1.212){alphaD <- "Not significant!"}
+  if(testStat$dStat > 1.212){alphaD <- "*"}
+  if(testStat$dStat > 1.330){alphaD <- "**"}
+  if(testStat$dStat > 1.569){alphaD <- "***"}
+  
+  # print:
+  # - critical value
+  # - test statistic name
+  # - how stat was calculated
+  # - significance
+  
+  cat("\n Critical Values \n")
+  
+  print(data.frame("Statistic"=c("Leemis' m", "Cho-Gains' d"),
+                   "Value"=c(testStat$mStat, testStat$dStat),
+                   "Alpha"=c(alphaM, alphaD),
+                   "Calculation"=c("max(freq - log10(1+1/1:9))", "sqrt(sum((freq - log10(1+1/1:9)^2)))")))
+  
+  # print legend explanation
+  cat("\n Note: p < 0.1^{*}; p < 0.05^{**}; p < 0.01^{***} \n")
+  
+  # export function output to csv file
+  sink("exportTable.csv", append=TRUE)
+  
+}
+
+# set working directory and execute function
+setwd("~/Google Drive/WashU/Spring2016/appliedStats")
+print.benfords2(x2)
